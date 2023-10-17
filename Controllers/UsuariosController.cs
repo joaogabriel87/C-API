009fdbb77578254fd80181f7e055c154fa2fe164
joaogabriel87 <joagabriel9@gmail.com>
2023-10-17 16:46:54 -0300

    Adição da autenticação e autorização no usuario

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -5,6 +5,11 @@ using System.Threading.Tasks;
 using Exo_WebApi.Models;
 using Exo_WebApi.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
 
 namespace Exo_WebApi.Controllers
 {
@@ -14,7 +19,7 @@ namespace Exo_WebApi.Controllers
     public class UsuariosController : ControllerBase
     {
         private readonly UsuarioRepository _usuarioRepository;
-        public UsuariosController(UsuarioRepository usuarioRepository) 
+        public UsuariosController(UsuarioRepository usuarioRepository)
         {
             _usuarioRepository = usuarioRepository;
         }
@@ -24,13 +29,49 @@ namespace Exo_WebApi.Controllers
         {
             return Ok(_usuarioRepository.Listar());
         }
-
-        [HttpPost]
-        public IActionResult Cadastrar(Usuario usuario)
+        // [HttpPost]
+        // public IActionResult Cadastrar(Usuario usuario)
+        // {
+        //     _usuarioRepository.Cadastrar(usuario);
+        //     return StatusCode(201);
+        // }
+        public IActionResult Post(Usuario usuario)
         {
-            _usuarioRepository.Cadastrar(usuario);
-            return StatusCode(201);
+            Usuario usuarioBuscado = _usuarioRepository.Login(usuario.Email,
+            usuario.Senha);
+            if (usuarioBuscado == null)
+            {
+                return NotFound("E-mail ou senha inválidos!");
+            }
+            // Se o usuário for encontrado, segue a criação do token.
+            // Define os dados que serão fornecidos no token - Payload.
+            var claims = new[]
+            {
+// Armazena na claim o e-mail usuário autenticado.
+new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
+// Armazena na claim o id do usuário autenticado.
+new Claim(JwtRegisteredClaimNames.Jti,
+usuarioBuscado.Id.ToString()),
+};
+            // Define a chave de acesso ao token.
+            var key = new
+            SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chaveautenticacao"));
+            // Define as credenciais do token.
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            // Gera o token.
+            var token = new JwtSecurityToken(
+            issuer: "exoapi.webapi", // Emissor do token.
+            audience: "exoapi.webapi", // Destinatário do token.
+            claims: claims, // Dados definidos acima.
+            expires: DateTime.Now.AddMinutes(30), // Tempo de expiração.
+            signingCredentials: creds // Credenciais do token.
+            );
+            // Retorna ok com o token.
+            return Ok(
+            new { token = new JwtSecurityTokenHandler().WriteToken(token) }
+            );
         }
+
 
         [HttpGet("{id}")]
         public IActionResult BuscarporId(int id)
@@ -42,13 +83,14 @@ namespace Exo_WebApi.Controllers
             }
             return Ok(usuario);
         }
+        [Authorize]
         [HttpPut("{id}")]
         public IActionResult Atualizar(int id, Usuario usuario)
         {
             _usuarioRepository.Atualizar(id, usuario);
             return StatusCode(204);
         }
-
+        [Authorize]
         [HttpDelete("{id}")]
         public IActionResult Deletar(int id)
         {
@@ -59,10 +101,10 @@ namespace Exo_WebApi.Controllers
             }
             catch (Exception e)
             {
-                
-              return BadRequest();
+
+                return BadRequest();
             }
         }
-    }   
+    }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,42 @@
 using Exo_WebApi.Context;
 using Exo_WebApi.Repositories;
+using Microsoft.IdentityModel.Tokens;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<ExoContext, ExoContext>();
 builder.Services.AddControllers();
+
+// Forma de autenticacão.
+builder.Services.AddAuthentication(options =>
+{
+options.DefaultAuthenticateScheme = "JwtBearer";
+options.DefaultChallengeScheme = "JwtBearer";
+})
+// Parâmetros de validacão do token.
+.AddJwtBearer("JwtBearer", options =>
+{
+options.TokenValidationParameters = new TokenValidationParameters
+{
+// Valida quem está solicitando.
+ValidateIssuer = true,
+// Valida quem está recebendo.
+ValidateAudience = true,
+// Define se o tempo de expiração será validado.
+ValidateLifetime = true,
+// Criptografia e validação da chave de autenticacão.
+IssuerSigningKey = new
+SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chaveautenticacao")),
+// Valida o tempo de expiração do token.
+ClockSkew = TimeSpan.FromMinutes(30),
+// Nome do issuer, da origem.
+ValidIssuer = "exoapi.webapi",
+// Nome do audience, para o destino.
+ValidAudience = "exoapi.webapi"
+};
+});
 builder.Services.AddTransient<ProjetoRepository, ProjetoRepository>();
 builder.Services.AddTransient<UsuarioRepository, UsuarioRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,6 +54,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
