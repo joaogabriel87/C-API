662bd1b0cb205b1fd2b4f342b1cbfacdb372e0a3
joaogabriel87 <joagabriel9@gmail.com>
2023-10-17 01:34:44 -0300

    Adição do metodo POST,PUT e DELETE da API

--- Repositories/ProjetoRepository.cs
+++ Repositories/ProjetoRepository.cs
@@ -18,5 +18,32 @@ namespace Exo_WebApi.Repositories
         {
             return _context.Projetos.ToList();
         }
+        public void Cadastrar(Projeto projeto)
+        {
+            _context.Projetos.Add(projeto);
+            _context.SaveChanges();
+        }
+        public Projeto BuscarporId(int id)
+        {
+            return _context.Projetos.Find(id);
+        }
+        public void Atualizar(int id, Projeto projeto)
+        {
+            Projeto projetoBuscado = _context.Projetos.Find(id);
+            if (projetoBuscado != null)
+            {
+                projetoBuscado.NomeDoProjeto = projeto.NomeDoProjeto;
+                projetoBuscado.Status = projeto.Status;
+                projetoBuscado.Area = projeto.Area;
+            }
+            _context.Projetos.Update(projetoBuscado);
+            _context.SaveChanges();
+        }
+        public void Deletar(int id)
+        {
+            Projeto projetoBuscado = _context.Projetos.Find(id);
+            _context.Projetos.Remove(projetoBuscado);
+            _context.SaveChanges();
+        }
     }
 }
